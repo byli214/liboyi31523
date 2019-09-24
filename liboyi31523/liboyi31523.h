064de40b5f3ef3458d6ddecefb092bb51844cbc5
liboyi <liboyi@moyi365.com>
2019-09-24 21:14:47 +0800

    31523

---
+++ liboyi31523/liboyi31523.h
@@ -0,0 +1,19 @@
+//
+//  liboyi31523.h
+//  liboyi31523
+//
+//  Created by ek on 2019/9/24.
+//  Copyright © 2019 ek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for liboyi31523.
+FOUNDATION_EXPORT double liboyi31523VersionNumber;
+
+//! Project version string for liboyi31523.
+FOUNDATION_EXPORT const unsigned char liboyi31523VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <liboyi31523/PublicHeader.h>
+
+
---
+++ liboyi31523/multiplyTwo.swift
@@ -0,0 +1,10 @@
+//
+//  multiplyTwo.swift
+//  liboyi31523
+//
+//  Created by ek on 2019/9/24.
+//  Copyright © 2019 ek. All rights reserved.
+//
+
+import Foundation
+public func multiply2(_ i: Int) -> Int { return i * 2 }
